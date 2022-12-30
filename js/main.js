9bca85f86b8fb5d8d6f7b3456788b3e899070f67
Jonathan <jonathan2887@hotmail.com>
2022-12-29 21:28:39 -0300

    cambios generales

--- js/main.js
+++ js/main.js
@@ -8,60 +8,54 @@
          let total = 0;
          let menu;
          do {
-             menu = parseInt(prompt("¡Bienvenido a JCBikes! \nPor favor, indique el tipo de operacion\n1-Comprar Nueva\n2-Comprar Usado\n3-Salir"));
-             
+             menu = prompt("¡Bienvenido a JCBikes! \n Por favor, indique el tipo de operacion\n 1-Comprar Nueva\n 2-Comprar Usado\n 3-Carrito de compras ");
+             menu = parseInt (menu);
              switch (menu) {
-                 case 1:
+                case 1:
                      total = total + comprarBici();
                      break;
-                 case 2:
+                case 2:
                     total = total + comprarUsado();
                      break;
-                 case 3: 
-                 alert ("Hasta luego.");
+                case 3: 
+                 alert ("El total acumulado es " + total);
                      break;
                 default:
                  alert ("La opción ingresa no es válida.");
              }
-         } while (menu != 3);
+         } while (menu != 4);
      }
 
 
 function comprarBici () {
     let acumulador = 0;
     alert("Por favor, seleccione el modelo que desea adquirir")
-    alert("1-Specialized Epic Pro\n2-Specialized Tarmac\n3-Specialized Diverge\n4-Specialized Rockhopper")
-    let tipo =parseInt(prompt("Ingreselo aqui"))
+    alert("1-Specialized Epic Pro\n2-Specialized Tarmac\n3-Specialized Diverge")
+    let tipo = prompt("Ingreselo aqui")
+    tipo = parseInt (tipo)
     switch (tipo) {
-        case 1: 
-            acumulador = acumulador + 1200000
+        case 1: acumulador = acumulador + 1200000
         break
-        case 2:
-            acumulador = acumulador + 950000
+        case 2: acumulador = acumulador + 950000
         break
-        case 3: 
-            acumulador = acumulador + 1100000
+        case 3: acumulador = acumulador + 1100000
         break
-        case 4:
-            acumulador = acumulador + 450000
-        break
-        default:
-            alert ("opcion invalida")
+        default: alert ("opcion invalida")
     }
     return acumulador
 }
+
 
 function comprarUsado () {
     let acumulador = 0;
     alert ("por favor, seleccione el modelo que desea adquirir")
     alert ("1-Zenith Astra\n2-Mondraker Chrono RR")
-    let tipo = parseInt(prompt("Ingreselo aqui"))
+    let tipo = prompt("Ingreselo aqui")
+    tipo = parseInt (tipo)
     switch (tipo) {
-        case 1:
-            190000
+        case 1: acumulador = acumulador + 190000
             break
-        case 2: 
-            650000
+        case 2: acumulador = acumulador + 650000
             break
         default:
             alert ("opcion invalida")
@@ -72,3 +66,4 @@ function comprarUsado () {
 menuPrincipal ()
 
 
+ 
