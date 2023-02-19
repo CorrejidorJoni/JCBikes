48cfc73c3f2791330fbb34f4e5bb9d87e4fc483b
Jonathan <jonathan2887@hotmail.com>
2023-02-19 17:09:00 -0300

    entrega final

--- js/carrito.js
+++ js/carrito.js
@@ -1,18 +1,129 @@
-const productosCarrito = JSON.parse(localStorage.getItem("productos-carrito"));
-console.log(productosCarrito)
+ let productosCarrito = localStorage.getItem("productos-carrito");
+ productosCarrito = JSON.parse(productosCarrito);
 
 
- const contenedorCarritoVacio = document.querySelector("#carrito-vacio");
- const contenedorCarritoProductos = document.querySelector("#carrito-productos");
- const contenedorCarritoAcciones = document.querySelector("#carrito-acciones");
- const contenedorCarritoComprado = document.querySelector("#carrito-comprado");
+  const contenedorCarritoVacio = document.querySelector("#carrito-vacio");
+  const contenedorCarritoProductos = document.querySelector("#carrito-productos");
+  const contenedorCarritoAcciones = document.querySelector("#carrito-acciones");
+  const contenedorCarritoComprado = document.querySelector("#carrito-comprado");
+  let botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar")
+  const botonVaciar = document.querySelector("#carrito-acciones-vaciar");
+  const contenedorTotal = document.querySelector("#total");
+  const botonComprar = document.querySelector("#carrito-acciones-comprar");
+  
+  function cargarProductosCarrito() {
+
+    if (productosCarrito && productosCarrito.length > 0) {
+        
+        contenedorCarritoVacio.classList.add("disabled");
+        contenedorCarritoProductos.classList.remove("disabled");
+        contenedorCarritoAcciones.classList.remove("disabled");
+        contenedorCarritoComprado.classList.add("disabled");
+        
+    
+        contenedorCarritoProductos.innerHTML = "";
+    
+        productosCarrito.forEach(producto => {
+    
+            const div = document.createElement("div");
+            div.classList.add("carrito-producto");
+            div.innerHTML= `
+            <img class="carrito-producto-imagen" src="${producto.imagen}" alt="${producto.modelo}">
+                <div class="carrito-producto-titulo">
+                    <small>Modelo</small>
+                    <h3>${producto.modelo}</h3>
+                </div>
+                <div class="carrito-producto-cantidad">
+                    <small>Cantidad</small>
+                    <p>${producto.cantidad}</p>
+                </div>
+                <div class="carrito-prodcuto-precio">
+                    <small>Precio</small>
+                    <p>${producto.precio}</p>
+                </div>
+                <div class="carrito-producto-subtotal">
+                    <small>Subtotal</small>
+                    <p>${prodcuto.precio * producto.cantidad}</p>
+                </div>
+                <button class="carrito-producto-eliminar" id= "${producto.id}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                  </svg></button>
+    
+                  `;
+                  
+            contenedorCarritoProductos.append(div);
+        
+        })
+    
+        
+    
+    } else {
+    
+        contenedorCarritoVacio.classList.remove("disabled");
+        contenedorCarritoProductos.classList.add("disabled");
+        contenedorCarritoAcciones.classList.add("disabled");
+        contenedorCarritoComprado.classList.add("disabled");
+    
+    }
+    refreshBotonesEliminar();
+    actualizarTotal ();
+  }
+  
+  cargarProductosCarrito();
+  
+  
+function refreshBotonesEliminar () {
+    botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar");
 
 
- if (productosEnCarrito) {
+    botonesEliminar.forEach(boton => {
+        boton.addEventListener("click", eliminarDelCarrito);
+    });
 
-    contenedorCarritoVacio.classList.add("disabled");
-     contenedorCarritoProductos.classList.remove("disabled");
-     contenedorCarritoAcciones.classList.remove("disabled");
-     contenedorCarritoComprado.classList.add("disabled");
+}
 
+function eliminarDelCarrito(e) {
+
+    const idBoton = e.currentTarget.id;
+    const index = productosCarrito.findIndex(producto => producto.id ===idBoton);
+    productosCarrito.splice(index, 1);
+    cargarProductosCarrito();
+
+    localStorage.setItem("productos-carrito", JSON.stringify(productosCarrito));
+}
+
+
+botonVaciar.addEventListener("click", vaciarCarrito);
+function vaciarCarrito () {
+
+    productosCarrito.length = 0;
+    localStorage.setItem("productos-carrito", JSON.stringify(productosCarrito));
+    cargarProductosCarrito();
+}
+
+
+function actualizarTotal () {
+    const totalCalculado = productosCarrito.reduce((acc, producto) => acc + (producto.precio * producto.cantidad),0)
+    actualizarTotal.innerText = `$${totalCalculado}`;
+}
+
+
+
+botonComprar.addEventListener("click", comprarCarrito);
+function comprarCarrito () {
+
+    Swal.fire({
+        icon: 'success',
+        title: '¡Compra exitosa!',
+        text: 'Nos cumunicaremos a la brevedad. Muchas gracias',
+        
+      })
+
+    productosCarrito.length = 0;
+    localStorage.setItem("productos-carrito", JSON.stringify(productosCarrito));
+    
+    contenedorCarritoVacio.classList.remove("disabled");
+    contenedorCarritoProductos.classList.add("disabled");
+    contenedorCarritoAcciones.classList.add("disabled");
+    contenedorCarritoComprado.classList.remove("disabled");
 }
--- js/main.js
+++ js/main.js
@@ -1,165 +1,11 @@
-///PRODUCTOS
- const productos = [
-    ///MTB
-    {
-        
-        modelo: "Epic Pro",
-        imagen: "./img/Epic_Pro_2022_SatinCarbon_01.jpg",
-        categoria: {
-            nombre: "mtb",
-            id: "mtb",
-        },
-        precio: 1850000,
-        id: "mtb-1",
-    },
-    {
-        modelo: "Epic Expert",
-        imagen: "./img/Specialized_Epic_Expert_2021_GlossRedTint_01.jpg",
-        categoria: {
-            nombre: "mtb",
-            id: "mtb",
-        },
-        precio: 2100000,
-        id: "mtb-2",
-    },
-    {
-        modelo: "Epic Expert",
-        imagen: "./img/Specialized_Epic_Expert_2021_SatinCarbon_01.jpg",
-        categoria: {
-            nombre: "mtb",
-            id: "mtb",
-        },
-        precio: 2100000,
-        id: "mtb-3",
-    },
-    {
-        modelo: "Epic Expert New",
-        imagen: "./img/Specialized_EpicExpert2022_GlossPine_01.jpg",
-        categoria: {
-            nombre: "mtb",
-            id: "mtb",
-        },
-        precio: 2400000,
-        id: "mtb-4",
-    },
+let productos = [];
 
-    ///ROAD
-    {
-        modelo: "Roubaix Sport",
-        imagen: "./img/Specialized_Roubaix_Sport_SatinFlakeSilver_01.jpg",
-        categoria: {
-            nombre: "road",
-            id: "road",
-        },
-        precio: 1400000,
-        id: "ruta-1",
-    },
-    {
-        modelo: "Tarmac SL6 Sport",
-        imagen: "./img/Specialized_Tarmac_SL6_Sport_Oasis_01.jpg",
-        categoria: {
-            nombre: "road",
-            id: "road",
-        },
-        precio: 980000,
-        id: "ruta-2",
-    },
-    {
-        modelo: "Tarmac Sworks SL7",
-        imagen: "./img/Sworks_Tarmarc_SL7_Shimano_RedTint_01.jpg",
-        categoria: {
-            nombre: "road",
-            id: "road",
-        },
-        precio: 3760000,
-        id: "ruta-3",
-    },
-    {
-        modelo: "Tarmac SL7 Expert",
-        imagen: "./img/Tarmac_SL7_Expert_Marron_01.jpg",
-        categoria: {
-            nombre: "road",
-            id: "road",
-        },
-        precio: 1200000,
-        id: "ruta-4",
-    },
-
-    ///GRAVEL
-    {
-        modelo: "Diverge Expert Carbon",
-        imagen: "./img/Diverge_Expert_Carbon_GlossRaspberry01.jpg",
-        categoria: {
-            nombre: "gravel",
-            id: "gravel",
-        },
-        precio: 1180000,
-        id: "gravel-1",
-    },
-    {
-        modelo: "Diverge Comp Carbon",
-        imagen: "./img/DivergeCompCarbon2022_Gunmetal_01.jpg" ,
-        categoria: {
-            nombre: "gravel",
-            id: "gravel",
-        },
-        precio: 995000,
-        id: "gravel-2",
-    },
-    {
-        modelo: "Diverge Sport Carbon",
-        imagen: "./img/DivergeSportCarbon_GlossWhiteSage_01.jpg",
-        categoria: {
-            nombre: "gravel",
-            id: "gravel",
-        },
-        precio: 870000,
-        id: "gravel-3",
-    },
-    {
-        modelo: "Diverge Sworks",
-        imagen: "./img/S-Works_Diverge_GlossLightSilver_01.jpg",
-        categoria: {
-            nombre: "gravel",
-            id: "gravel",
-        },
-        precio: 3400000,
-        id: "gravel-4",
-    },
-
-    ///E-Bike
-    {
-        modelo: "Turbo Levo SL Expert",
-        imagen: "./img/Specialied_Turbo_Levo_SL_Expert_CarbonWhite_01.jpg",
-        categoria: {
-            nombre: "ebike",
-            id: "ebike",
-        },
-        precio: 2700000,
-        id: "ebike-1",
-    },
-    {
-        modelo: "Turbo Levo Comp",
-        imagen: "./img/Specialized_Turbo_Levo_Comp_Carbon_2022_Black_01.jpg",
-        categoria: {
-            nombre: "ebike",
-            id: "ebike",
-        },
-        precio: 2200000,
-        id: "ebike-2",
-    },
-    {
-        modelo: "Turbo Levo Alloy", 
-        imagen: "./img/Turbo_Levo_Comp_Alloy_2022_SageGreen_01.jpg",
-        categoria: {
-            nombre: "ebike",
-            id: "ebike",
-        },
-        precio: 1800000,
-        id: "ebike-3",
-    }
-
-];
+fetch("./js/productos.json")
+.then(response => response.json())
+.then(data => {
+    productos = data;
+    cargarProductos(productos);
+})
 
 const contenedorProductos = document.querySelector("#contenedor-productos");
 const botonesCategorias = document.querySelectorAll(".boton-categoria");
@@ -191,7 +37,7 @@ function cargarProductos (productosElegidos) {
     
 }
 
-cargarProductos(productos);
+
 
 botonesCategorias.forEach(boton => {
     boton.addEventListener("click", (e) =>{
@@ -223,9 +69,30 @@ botonesCategorias.forEach(boton => {
      });
  
 }
- const productosCarrito = [];
+
+let productosCarrito;
+
+let productosCarritoLS = localStorage.getItem("prodcutos-carrito");
+
+
+if (productosCarritoLS) {
+    productosCarrito = JSON.parse(productosCarritoLS);
+    actualizarNumero();
+} else {
+
+    productosCarrito = [];
+}
+
 
  function agregarAlCarrito(e) {
+
+   
+Swal.fire({
+    icon: 'success',
+    title: '¡Buena elección!',
+    text: 'El producto se agregó al carrito',
+    
+  })
 
      const idBoton = e.currentTarget.id;
      const productoAgregado = productos.find(producto => producto.id === idBoton );
@@ -279,6 +146,4 @@ botonesCategorias.forEach(boton => {
 
 
 
-
-
-
+ 
